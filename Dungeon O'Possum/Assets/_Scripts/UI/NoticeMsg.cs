cb666046f7fcf082e027840d1c6d993560297207
Audrey <brooksaudrey95@gmail.com>
2025-05-13 13:07:35 -0500

    fixed weird buggout with files
    for some reason a bunch of double files were introduced and other bs. Fixed it I think, could be more problems so this may be bugfix#1

--- Dungeon O'Possum/Assets/_Scripts/UI/NoticeMsg.cs
+++ Dungeon O'Possum/Assets/_Scripts/UI/NoticeMsg.cs
@@ -5,27 +5,29 @@ using UnityEngine.UI;
 using TMPro;
 
 public class NoticeMsg : MonoBehaviour  {
-    [SerializeField] TextMeshProUGUI text;
-
     
-    /*
-     * 
-     */
-    void Start()  {
-        HideText();
-    }
+    [SerializeField] TextMeshProUGUI text;  //our TextMeshPro reference
+    [SerializeField] private string textToShow;
 
-    /*
-     * 
-     */
-    public void ShowText()  {
-        text.gameObject.SetActive(true);
-    }
+    //on Awake: hide Notice Text
+    void Awake() { HideText(); }
 
-    /*
-     * 
-     */
-    public void HideText(){
-        text.gameObject.SetActive(false);
+
+    //Show Notice Text - and update text
+    public void ShowText(string textToShow) {
+        UpdateText(textToShow);             //update text
+        text.gameObject.SetActive(true);    //set the reference to active state
     }
+    
+
+    //update what text will say
+    public void UpdateText(string textToShow) { 
+        this.textToShow = textToShow; 
+        text.text = textToShow;
+    }
+    
+
+    //Hide Notice Text
+    public void HideText(){ text.gameObject.SetActive(false); }  //set the reference to inactive state
+       
 }
