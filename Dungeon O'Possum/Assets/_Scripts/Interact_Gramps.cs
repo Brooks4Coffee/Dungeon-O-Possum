32360fba67177669a5c2664f5f86f9c9b4ac7091
Audrey <brooksaudrey95@gmail.com>
2025-05-15 17:31:20 -0500

    small changes
    xyz

---
+++ Dungeon O'Possum/Assets/_Scripts/Interact_Gramps.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact_Gramps : Interact  {
+
+    [SerializeField] GameManager gm; //store bool that we talked to him
+    [SerializeField] string d1;         //first dialogue said
+    [SerializeField] string d2;         //second dialogue said
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
